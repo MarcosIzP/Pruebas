2c365d76e6c465dd5b90bf37842ab86e567c7b5a
MarcosIzP <marcosasir28@gmail.com>
2022-05-06 20:18:32 +0200

    Inicio de la pantalla de autenticación

--- app/src/main/java/com/tfg/aplicacio/AuthActivity.kt
+++ app/src/main/java/com/tfg/aplicacio/AuthActivity.kt
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
 
         //Analiticas
         val analytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
         bundle.putString("mensaje", "Integracion de Firebase Analytics")
         analytics.logEvent("PantallaInicio", bundle)
+
+
     }
 }
